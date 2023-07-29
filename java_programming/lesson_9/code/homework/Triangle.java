a161c662380b44f55cb054655a66b5ecdfe0d89e
Егор Кнестяпин <knesp@yandex.ru>
2023-07-29 23:33:47 +0400

    fixed triangle

--- java_programming/lesson_9/code/homework/Triangle.java
+++ java_programming/lesson_9/code/homework/Triangle.java
@@ -10,13 +10,6 @@ public class Triangle {
         12345
         123456
          */
-//        String triangle = "";
-//        int a = 1;
-//        int b = 6;
-//        for (int i = a; i <= b; i++) {
-//            triangle += i;
-//            System.out.println(triangle);
-//        }
         int n = 6;
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= i; j++) {
