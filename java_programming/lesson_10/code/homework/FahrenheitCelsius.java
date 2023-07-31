02054759b629a48e426b8b45bd62205a954c2077
Егор Кнестяпин <knesp@yandex.ru>
2023-07-31 22:31:15 +0400

    fixed fahrenheitcelsius

--- java_programming/lesson_10/code/homework/FahrenheitCelsius.java
+++ java_programming/lesson_10/code/homework/FahrenheitCelsius.java
@@ -4,9 +4,9 @@ public class FahrenheitCelsius {
     public static void main(String[] args) {
         // Написать метод для перевода градусов по фаренгейту в шкалу цельсия. Формула для расчета С = 5*(F-32)/9.
         // Результат вывести на экран.
-        switchFromFahrenheitToCelsius(67.5); // 19,7
+        convertFromFahrenheitToCelsius(67.5); // 19,7
     }
-    static void switchFromFahrenheitToCelsius(double F) {
+    static void convertFromFahrenheitToCelsius(double F) {
         double C = 5 * (F - 32) / 9;
         System.out.printf("%.1f", C);
     }
