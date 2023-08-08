161c9e3996e554dd51c56c7c4508530bf071f722
Егор Кнестяпин <knesp@yandex.ru>
2023-08-08 11:53:12 +0400

    fixed daymonthyear

--- java_programming/lesson_16/code/lesson/DayMonthYear.java
+++ java_programming/lesson_16/code/lesson/DayMonthYear.java
@@ -3,14 +3,13 @@ package lesson;
 public class DayMonthYear {
     public static void main(String[] args) {
         // Написать метод,, который принимает 3 числа int (day, month, year) возвращает строку вида "08. August 2023"
-        System.out.println(getStringDayMonthYear(12,1,2023));
+        System.out.println(getStringDayMonthYear(8, 8, 2023));
     }
 
     static String getStringDayMonthYear(int day, int month, int year) {
         String[] months = {"January", "February", "March", "April", "May", "June", "July", "August", "September",
                 "October", "November", "December"};
         String dayString = day > 10 ? "" + day : "0" + day;
-        String yearString = "" + year;
-        return dayString + ". " + months[month - 1] + " " + yearString;
+        return dayString + ". " + months[month - 1] + " " + year;
     }
 }
