3952a60cdfc026b9b1a70ecfa89bc6f4b708f5be
Егор Кнестяпин <knesp@yandex.ru>
2023-08-21 18:58:16 +0400

    fixed user constructor

--- java_programming/lesson_24/src/ait/user/model/User.java
+++ java_programming/lesson_24/src/ait/user/model/User.java
@@ -6,7 +6,7 @@ public class User {
 
     public User(String email, String password) {
         setEmail(email);
-        this.password = password;
+        setPassword(password);
     }
 
     public String getEmail() {
