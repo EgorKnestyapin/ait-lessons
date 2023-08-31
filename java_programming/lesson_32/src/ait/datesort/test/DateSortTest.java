d70e9c339c148a7fb134ad5bedadb8d0909e812b
Егор Кнестяпин <knesp@yandex.ru>
2023-08-31 16:27:04 +0400

    added testdatesort2

--- java_programming/lesson_32/src/ait/datesort/test/DateSortTest.java
+++ java_programming/lesson_32/src/ait/datesort/test/DateSortTest.java
@@ -10,6 +10,7 @@ import java.util.Comparator;
 
 public class DateSortTest {
     Comparator<String> dateComparator;
+    Comparator<String> dateComparator2;
 
     @BeforeEach
     void setUp() {
@@ -40,6 +41,23 @@ public class DateSortTest {
                 }
             }
         };
+        dateComparator2 = (s1, s2) -> {
+            int[] arrDate1 = new int[3];
+            int[] arrDate2 = new int[3];
+            for (int i = 0; i < 3; i++) {
+                arrDate1[i] = Integer.parseInt(s1.split("-")[i]);
+                arrDate2[i] = Integer.parseInt(s2.split("-")[i]);
+            }
+            int date1 = arrDate1[0] + arrDate1[1] * 100 + arrDate1[2] * 10_000;
+            int date2 = arrDate2[0] + arrDate2[1] * 100 + arrDate2[2] * 10_000;
+            if (date1 > date2) {
+                return 1;
+            } else if (date1 < date2) {
+                return -1;
+            } else {
+                return 0;
+            }
+        };
     }
 
     @Test
@@ -59,6 +77,26 @@ public class DateSortTest {
                 "28-01-2010"
         };
         Arrays.sort(dates, dateComparator);
+        assertArrayEquals(expected, dates);
+    }
+
+    @Test
+    void testDateSort2() {
+        String[] dates = {
+                "07-05-1990",
+                "28-01-2010",
+                "11-08-1990",
+                "15-01-2010",
+                "16-06-1970"
+        };
+        String[] expected = {
+                "16-06-1970",
+                "07-05-1990",
+                "11-08-1990",
+                "15-01-2010",
+                "28-01-2010"
+        };
+        Arrays.sort(dates, dateComparator2);
         assertArrayEquals(expected, dates);
     }
 }
