5c2c1b158846b2e8f407b8ced6c9de5ddd134ac4
Егор Кнестяпин <knesp@yandex.ru>
2023-09-19 14:04:08 +0400

    added additional methods

--- java_programming/lesson_44/src/homework/Main.java
+++ java_programming/lesson_44/src/homework/Main.java
@@ -1,9 +1,6 @@
 package homework;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
@@ -16,9 +13,9 @@ public class Main {
     которые в сумме дают заданное число N.
      */
     public static boolean findPairNumSum1(List<Integer> list, int N) {
-        List<Integer> res = new ArrayList<>(list);
-        for (int i = 0; i < res.size() - 1; i++) {
-            boolean isContains = res.contains(N - res.get(i));
+        if (list == null || list.size() < 2) return false;
+        for (int i = 0; i < list.size() - 1; i++) {
+            boolean isContains = list.contains(N - list.get(i));
             if (isContains) {
                 return true;
             }
@@ -27,13 +24,46 @@ public class Main {
     }
 
     public static boolean findPairNumSum2(List<Integer> list, int N) {
-        List<Integer> res = new ArrayList<>(list);
-        for (int i = 0; i < res.size() - 1; i++) {
-            for (int j = i + 1; j < res.size(); j++) {
-                int sum = res.get(i) + res.get(j);
+        if (list == null || list.size() < 2) return false;
+        for (int i = 0; i < list.size() - 1; i++) {
+            for (int j = i + 1; j < list.size(); j++) {
+                int sum = list.get(i) + list.get(j);
                 if (sum == N) {
                     return true;
                 }
+            }
+        }
+        return false;
+    }
+
+    public static boolean findPairNumSum3(List<Integer> list, int N) {
+        if (list == null || list.size() < 2) return false;
+        Set<Integer> set = new HashSet<>();
+        for (int i = 0; i < list.size(); i++) {
+            if (set.contains(N - list.get(i))) {
+                return true;
+            }
+            set.add(N - list.get(i));
+        }
+        return false;
+    }
+
+    public static boolean findPairNumSum4(List<Integer> list, int N) {
+        if (list == null || list.size() < 2) return false;
+        Collections.sort(list);
+        int lIndex = 0;
+        int rIndex = list.size() - 1;
+        while (lIndex < rIndex) {
+            int lValue = list.get(lIndex);
+            int rValue = list.get(rIndex);
+            int sum = lValue + rValue;
+            if (sum == N) {
+                return true;
+            }
+            if (sum < N) {
+                lIndex++;
+            } else {
+                rIndex--;
             }
         }
         return false;
