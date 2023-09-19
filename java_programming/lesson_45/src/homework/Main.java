265b945bdcb7d3587f14ba772e5bca7d3fa5f035
Егор Кнестяпин <knesp@yandex.ru>
2023-09-19 20:20:30 +0400

    added lesson45

---
+++ java_programming/lesson_45/src/homework/Main.java
@@ -0,0 +1,44 @@
+package homework;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class Main {
+    public static void main(String[] args) {
+        String seq1 = "( {} [] )";
+        String seq2 = "( {} [ )";
+        String seq3 = "( } [] )";
+        String seq4 = "{ ( } [] )";
+        System.out.println(isBracketSequenceRight(seq1));
+        System.out.println(isBracketSequenceRight(seq2));
+        System.out.println(isBracketSequenceRight(seq3));
+        System.out.println(isBracketSequenceRight(seq4));
+    }
+    /* ** Задача 1 **
+    Дана строка - скобочная последовательность ( строка состоящая из скобок трех видов *() [] {} ).
+    Написать метод (алгоритм обсуждали в классе) который проверит правильность расстановки скобок.
+    ( {} [] ) -> true
+    ( {} [ ) -> false
+    ( } [] ) -> false
+    { ( } [] ) -> false
+     */
+    public static boolean isBracketSequenceRight(String sequence) {
+        if (sequence == null) return false;
+        Deque<Character> stack = new LinkedList<>();
+        for (int i = 0; i < sequence.length(); i++) {
+            char ch = sequence.charAt(i);
+            if (ch == ' ') {
+            } else if (ch == '(') {
+                stack.push(')');
+            } else if (ch == '{') {
+                stack.push('}');
+            } else if (ch == '[') {
+                stack.push(']');
+            } else if (ch == stack.pop()) {
+            } else {
+                return false;
+            }
+        }
+        return true;
+    }
+}
---
+++ java_programming/lesson_45/src/lesson/Main.java
@@ -0,0 +1,28 @@
+package lesson;
+
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) {
+        Deque<Integer> deque = new LinkedList<>();
+        deque.addFirst(1);
+        deque.addFirst(2);
+        deque.addFirst(3);
+        deque.addFirst(4);
+        System.out.println(deque);
+        System.out.println("delete " + deque.removeLast());
+        System.out.println(deque);
+
+        // Stack
+        Deque<Integer> stack = new LinkedList<>();
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+
+        /*
+        TODO Дано (2
+         */
+    }
+}
